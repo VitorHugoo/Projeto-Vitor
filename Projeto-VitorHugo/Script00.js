4ea486d84643d1b986b1457b35a2270f0774b076
VitorHugoo <54085141+VitorHugoo@users.noreply.github.com>
2019-08-22 18:26:20 -0300

    Add files via upload

---
+++ Projeto-VitorHugo/Script00.js
@@ -0,0 +1,54 @@
+
+$(window).load(function(){
+
+
+		$("#Menu").(function(){
+		$("#panel").slideUp("fast");
+		$("#panel2").slideUp("fast");
+		$("#panel3").slideUp("fast");
+		$("#panel4").slideUp("fast");
+		$("#panel5").slideUp("fast");
+		$("#panel6").slideUp("fast");
+		$("#panel7").slideUp("fast");
+		$("#panel8").slideUp("fast");
+	});
+
+});
+
+$(document).ready(function(){
+
+
+		$("#flip").click(function(){
+		$("#panel").slideToggle("fast");
+	});
+
+		$("#flip2").click(function(){
+		$("#panel2").slideToggle("fast");
+	});
+
+		$("#flip3").click(function(){
+		$("#panel3").slideToggle("fast");
+	});
+
+		$("#flip4").click(function(){
+		$("#panel4").slideToggle("fast");
+	});
+
+		$("#flip5").click(function(){
+		$("#panel5").slideToggle("fast");
+	});
+
+		$("#flip6").click(function(){
+		$("#panel6").slideToggle("fast");
+	});
+
+		$("#flip7").click(function(){
+		$("#panel7").slideToggle("fast");
+	});
+
+		$("#flip8").click(function(){
+		$("#panel8").slideToggle("fast");
+	});
+
+
+});
---
+++ Scripts/Script00.js
@@ -0,0 +1,39 @@
+
+
+$(document).ready(function(){
+
+
+		$("#flip").click(function(){
+		$("#panel").slideToggle("fast");
+	});
+
+		$("#flip2").click(function(){
+		$("#panel2").slideToggle("fast");
+	});
+
+		$("#flip3").click(function(){
+		$("#panel3").slideToggle("fast");
+	});
+
+		$("#flip4").click(function(){
+		$("#panel4").slideToggle("fast");
+	});
+
+		$("#flip5").click(function(){
+		$("#panel5").slideToggle("fast");
+	});
+
+		$("#flip6").click(function(){
+		$("#panel6").slideToggle("fast");
+	});
+
+		$("#flip7").click(function(){
+		$("#panel7").slideToggle("fast");
+	});
+
+		$("#flip8").click(function(){
+		$("#panel8").slideToggle("fast");
+	});
+
+
+});
